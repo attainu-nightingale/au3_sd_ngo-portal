0f68c520ab45cb9ca66c623d5d20ad5c1292d606
Ruhan Khandakar <khandakarruhan@gmail.com>
2019-09-06 08:12:46 +0530

    setup initial express

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
+const path = require("path");
+const express = require("express");
+const hbs = require("hbs");
+
 const { port } = require("./config");
 
-console.log(`Server Running at ${port}`);
+// Paths
+const STATIC_PATH = path.join(__dirname, "./public");
+const VIEWS_PATH = path.join(__dirname, "/views/templates");
+const PARTIALS_PATH = path.join(__dirname, "/views/partials");
+
+// Initialize express
+const app = express();
+
+// hbs setup
+app.set("view engine", "hbs");
+app.set("views", VIEWS_PATH);
+hbs.registerPartials(PARTIALS_PATH);
+
+// express middleware
+app.use(express.static(STATIC_PATH));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Home route
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+// Start Server
+app.listen(port, () => {
+  console.log(`Server Running at ${port}`);
+});
